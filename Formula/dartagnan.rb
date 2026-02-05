a247ae2afd555cce35cac4fe8620fd6f240a463b
Lucca <luccajbecker@gmail.com>
2026-02-05 12:45:37 -0300

    fix: use scripts/dartagnan

--- Formula/dartagnan.rb
+++ Formula/dartagnan.rb
@@ -10,33 +10,30 @@ class Dartagnan < Formula
 
   def install
     ENV["JAVA_HOME"] = Formula["openjdk@17"].opt_prefix
-    
-    # Disable git-commit-id-plugin in all submodules
+
+    # Disable git-commit-id-plugin (tarball build has no .git)
     %w[dartagnan svcomp].each do |submodule|
       inreplace "#{submodule}/pom.xml" do |s|
-        s.gsub! /<execution>\s*<id>populate-git-commit-information<\/id>.*?<\/execution>/m, ""
+        s.gsub!(/<execution>\s*<id>populate-git-commit-information<\/id>.*?<\/execution>/m, "")
       end
     end
-    
+
     system "mvn", "clean", "install", "-DskipTests"
-    
-    # Install JAR and all dependencies
+
+    # Install runtime artifacts in libexec (Homebrew standard)
     libexec.install "dartagnan/target/dartagnan.jar"
-    libexec.install Dir["dartagnan/target/libs/*.jar"]
-    libexec.install Dir["dartagnan/target/libs/*.dylib"]
-    
-    # Install pom.xml for version information
-    libexec.install "dartagnan/pom.xml"
-    
-    # Create wrapper script
+    (libexec/"libs").install Dir["dartagnan/target/libs/*.jar"]
+    (libexec/"libs").install Dir["dartagnan/target/libs/*.dylib"]
+
+    # Install upstream script
+    bin.install "scripts/dartagnan" => "dartagnan.original"
+
+    # Wrapper that sets the expected environment
     (bin/"dartagnan").write <<~EOS
       #!/bin/bash
-      export JAVA_HOME="#{Formula["openjdk@17"].opt_prefix}"
-      exec "${JAVA_HOME}/bin/java" -Duser.home="#{libexec}" -cp "#{libexec}/dartagnan.jar:#{libexec}/*" com.dat3m.dartagnan.Dartagnan "$@"
+      export DAT3M_HOME="#{libexec}"
+      export DAT3M_JAVA="#{Formula["openjdk@17"].opt_prefix}/bin/java"
+      exec "#{bin}/dartagnan.original" "$@"
     EOS
-  end
-
-  test do
-    system "#{bin}/dartagnan", "--version"
   end
 end
